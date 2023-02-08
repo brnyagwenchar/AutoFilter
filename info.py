a6de721dcce114106c88eaaa3cee5142b4e0aab3
brnyagwenchar <124819926+brnyagwenchar@users.noreply.github.com>
2023-02-09 00:42:13 +0300

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "bot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TamilanBotsZ_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
